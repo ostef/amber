e500fac7efefdd14ad28f7a36f3329837305eb1a
ostef <47391841+ostef@users.noreply.github.com>
2022-05-01 19:24:58 +0200

    Brought back Meta/Build as the metaprogram.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
-jai source/build.jai -import_dir ../modules -- %*
+jai source/build.jai -import_dir ../modules -- %* import_dir ../modules meta Meta/Build
 rem jai source/build.jai -import_dir modules\ -import_dir c:\jai\modules\ -meta Meta/Build -debug
