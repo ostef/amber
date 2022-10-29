7e8437a6d96b176a71ff6f2b4f1827318f4d7136
ostef <47391841+ostef@users.noreply.github.com>
2022-10-29 15:02:27 +0200

    Fixed compile errors due to newest version of modules.

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 @echo off
 
-jai source/build.jai -import_dir ../modules -- %* import_dir ../modules meta Meta/Build
-rem jai source/build.jai -import_dir modules\ -import_dir c:\jai\modules\ -meta Meta/Build -debug
+jai -import_dir ../modules source/build.jai -- %*
